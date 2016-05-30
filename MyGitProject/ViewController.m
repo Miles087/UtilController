5b990267d81e39dc0e04564cc993726312a073c8
Miles Wang <Miles@MacBook-Air.local>
2016-05-30 15:11:59 +0800

    Create Project files

---
+++ MyGitProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyGitProject
+//
+//  Created by Miles Wang on 16/5/30.
+//  Copyright © 2016年 Miles Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MyGitiOSProject/MyGitiOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGitiOSProject
+//
+//  Created by Miles Wang on 16/5/30.
+//  Copyright © 2016年 Miles Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
